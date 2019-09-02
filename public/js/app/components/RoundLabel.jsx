e93bf74da58e7565cba5f0c368049ce20562e4bd
Jeanette Skoog <jeanetteskoog@n174-p254.eduroam.kth.se>
2019-09-02 08:53:17 +0200

    fix of round label without shortName

--- public/js/app/components/RoundLabel.jsx
+++ public/js/app/components/RoundLabel.jsx
@@ -7,7 +7,7 @@ const RoundLabel = ({round, semester, language}) =>{
     const translate = i18n.messages[language].messages
     let roundName =  round.shortName 
     ? round.shortName + ' '
-    : `${translate.course_short_semester[semester].toString().match(/.{1,4}/g)[1]} 
+    : `${translate.course_short_semester[semester.toString().match(/.{1,4}/g)[1]]} 
        ${semester.toString().match(/.{1,4}/g)[0]}-${round.roundId} `
 
     return (
