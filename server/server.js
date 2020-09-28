ee492ca0bed079daf359915a2e2d73739d7d9f55
Elena Rakhimova <elenara@kth.se>
2020-09-28 17:31:27 +0200

    fix: update server js

--- server/server.js
+++ server/server.js
@@ -4,11 +4,12 @@ const server = require('kth-node-server')
 const config = require('./configuration').server
 require('./api')
 const AppRouter = require('kth-node-express-routing').PageRouter
-const getPaths = require('kth-node-express-routing').getPaths
+const { getPaths } = require('kth-node-express-routing')
 
 if (config.appInsights && config.appInsights.instrumentationKey) {
   let appInsights = require('applicationinsights')
-  appInsights.setup(config.appInsights.instrumentationKey)
+  appInsights
+    .setup(config.appInsights.instrumentationKey)
     .setAutoDependencyCorrelation(true)
     .setAutoCollectRequests(true)
     .setAutoCollectPerformance(true)
@@ -37,7 +38,7 @@ let logConfiguration = {
   level: config.logging.log.level,
   console: config.logging.console,
   stdout: config.logging.stdout,
-  src: config.logging.src
+  src: config.logging.src,
 }
 log.init(logConfiguration)
 
@@ -50,11 +51,14 @@ const path = require('path')
 server.set('views', path.join(__dirname, '/views'))
 server.set('layouts', path.join(__dirname, '/views/layouts'))
 server.set('partials', path.join(__dirname, '/views/partials'))
-server.engine('handlebars', exphbs({
-  defaultLayout: 'publicLayout',
-  layoutsDir: server.settings.layouts,
-  partialsDir: server.settings.partials
-}))
+server.engine(
+  'handlebars',
+  exphbs({
+    defaultLayout: 'publicLayout',
+    layoutsDir: server.settings.layouts,
+    partialsDir: server.settings.partials,
+  })
+)
 server.set('view engine', 'handlebars')
 // Register handlebar helpers
 require('./views/helpers')
@@ -82,7 +86,7 @@ const express = require('express')
 // server.use(minify())
 
 // helper
-function setCustomCacheControl (res, path) {
+function setCustomCacheControl(res, path) {
   if (express.static.mime.lookup(path) === 'text/html') {
     // Custom Cache-Control for HTML files
     res.setHeader('Cache-Control', 'no-cache')
@@ -91,7 +95,10 @@ function setCustomCacheControl (res, path) {
 
 // Files/statics routes--
 // Map components HTML files as static content, but set custom cache control header, currently no-cache to force If-modified-since/Etag check.
-server.use(config.proxyPrefixPath.uri + '/static/js/components', express.static('./dist/js/components', { setHeaders: setCustomCacheControl }))
+server.use(
+  config.proxyPrefixPath.uri + '/static/js/components',
+  express.static('./dist/js/components', { setHeaders: setCustomCacheControl })
+)
 // Expose browser configurations
 server.use(config.proxyPrefixPath.uri + '/static/browserConfig', browserConfigHandler)
 // Map Bootstrap.
@@ -99,12 +106,11 @@ server.use(config.proxyPrefixPath.uri + '/static/bootstrap', express.static('./n
 // Map kth-style.
 server.use(config.proxyPrefixPath.uri + '/static/kth-style', express.static('./node_modules/kth-style/dist'))
 
-// server.use(config.proxyPrefixPath.uri + '/static/js/app.js', express.static('./dist/js/app.js'))
 // Map static content like images, css and js.
 server.use(config.proxyPrefixPath.uri + '/static', express.static('./dist'))
 // Return 404 if static file isn't found so we don't go through the rest of the pipeline
 server.use(config.proxyPrefixPath.uri + '/static', function (req, res, next) {
-  var error = new Error('File not found: ' + req.originalUrl)
+  const error = new Error('File not found: ' + req.originalUrl)
   error.statusCode = 404
   next(error)
 })
@@ -120,7 +126,7 @@ server.set('case sensitive routing', true)
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 server.use(bodyParser.json())
-server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 1000000 }))
 server.use(cookieParser())
 
 /* ***********************
@@ -145,11 +151,18 @@ server.use(config.proxyPrefixPath.uri, languageHandler)
  */
 const passport = require('passport')
 // const ldapClient = require('./adldapClient')
-const { authLoginHandler, authCheckHandler, logoutHandler, pgtCallbackHandler, serverLogin, getServerGatewayLogin } = require('kth-node-passport-cas').routeHandlers({
+const {
+  authLoginHandler,
+  authCheckHandler,
+  logoutHandler,
+  pgtCallbackHandler,
+  serverLogin,
+  getServerGatewayLogin,
+} = require('kth-node-passport-cas').routeHandlers({
   casLoginUri: config.proxyPrefixPath.uri + '/login',
   casGatewayUri: config.proxyPrefixPath.uri + '/loginGateway',
   proxyPrefixPath: config.proxyPrefixPath.uri,
-  server: server
+  server: server,
 })
 const { redirectAuthenticatedUserHandler } = require('./authentication')
 server.use(passport.initialize())
@@ -157,7 +170,12 @@ server.use(passport.session())
 
 const authRoute = AppRouter()
 authRoute.get('cas.login', config.proxyPrefixPath.uri + '/login', authLoginHandler, redirectAuthenticatedUserHandler)
-authRoute.get('cas.gateway', config.proxyPrefixPath.uri + '/loginGateway', authCheckHandler, redirectAuthenticatedUserHandler)
+authRoute.get(
+  'cas.gateway',
+  config.proxyPrefixPath.uri + '/loginGateway',
+  authCheckHandler,
+  redirectAuthenticatedUserHandler
+)
 authRoute.get('cas.logout', config.proxyPrefixPath.uri + '/logout', logoutHandler)
 // Optional pgtCallback (use config.cas.pgtUrl?)
 authRoute.get('cas.pgtCallback', config.proxyPrefixPath.uri + '/pgtCallback', pgtCallbackHandler)
@@ -171,12 +189,15 @@ server.gatewayLogin = getServerGatewayLogin
  * ******* CORTINA BLOCKS *******
  * ******************************
  */
-server.use(config.proxyPrefixPath.uri, require('kth-node-web-common/lib/web/cortina')({
-  blockUrl: config.blockApi.blockUrl,
-  proxyPrefixPath: config.proxyPrefixPath.uri,
-  hostUrl: config.hostUrl,
-  redisConfig: config.cache.cortinaBlock.redis
-}))
+server.use(
+  config.proxyPrefixPath.uri,
+  require('kth-node-web-common/lib/web/cortina')({
+    blockUrl: config.blockApi.blockUrl,
+    proxyPrefixPath: config.proxyPrefixPath.uri,
+    hostUrl: config.hostUrl,
+    redisConfig: config.cache.cortinaBlock.redis,
+  })
+)
 
 /* ********************************
  * ******* CRAWLER REDIRECT *******
@@ -184,9 +205,12 @@ server.use(config.proxyPrefixPath.uri, require('kth-node-web-common/lib/web/cort
  */
 const excludePath = config.proxyPrefixPath.uri + '(?!/static).*'
 const excludeExpression = new RegExp(excludePath)
-server.use(excludeExpression, require('kth-node-web-common/lib/web/crawlerRedirect')({
-  hostUrl: config.hostUrl
-}))
+server.use(
+  excludeExpression,
+  require('kth-node-web-common/lib/web/crawlerRedirect')({
+    hostUrl: config.hostUrl,
+  })
+)
 
 /* ********************************
  * ******* FILE UPLOAD*******
@@ -215,15 +239,47 @@ server.use('/', systemRoute.getRouter())
 
 // App routes
 const appRoute = AppRouter()
-appRoute.get('system.index', config.proxyPrefixPath.uri + '/:id', serverLogin, requireRole('isCourseResponsible', 'isExaminator', 'isSuperUser'), Admin.getIndex)
-appRoute.get('system.gateway', config.proxyPrefixPath.uri + '/gateway', getServerGatewayLogin('/'), requireRole('isAdmin'), Admin.getIndex)
+appRoute.get(
+  'system.index',
+  config.proxyPrefixPath.uri + '/:id',
+  serverLogin,
+  requireRole('isCourseResponsible', 'isExaminator', 'isSuperUser'),
+  Admin.getIndex
+)
+appRoute.get(
+  'system.gateway',
+  config.proxyPrefixPath.uri + '/gateway',
+  getServerGatewayLogin('/'),
+  requireRole('isAdmin'),
+  Admin.getIndex
+)
 
 appRoute.all('api.memoPost', config.proxyPrefixPath.uri + '/apicall/postMemoData/:id', Admin.postMemoData)
-appRoute.get('api.memoGetUsedRounds', config.proxyPrefixPath.uri + '/apicall/memoGetUsedRounds/:courseCode/:semester', Admin.getUsedRounds)
-appRoute.get('api.koppsCourseData', config.proxyPrefixPath.uri + '/api/memo-admin/getKoppsCourseDataByCourse/:courseCode/:language', Admin.getKoppsCourseData)
-appRoute.post('storage.saveFile', config.proxyPrefixPath.uri + '/storage/saveFile/:semester/:courseCode/:rounds', Admin.saveFileToStorage)
-appRoute.post('storage.updateFile', config.proxyPrefixPath.uri + '/storage/updateFile/:fileName/', Admin.updateFileInStorage)
-appRoute.post('storage.deleteFile', config.proxyPrefixPath.uri + '/storage/deleteFile/:fileName', Admin.deleteFileInStorage)
+appRoute.get(
+  'api.memoGetUsedRounds',
+  config.proxyPrefixPath.uri + '/apicall/memoGetUsedRounds/:courseCode/:semester',
+  Admin.getUsedRounds
+)
+appRoute.get(
+  'api.koppsCourseData',
+  config.proxyPrefixPath.uri + '/api/memo-admin/getKoppsCourseDataByCourse/:courseCode/:language',
+  Admin.getKoppsCourseData
+)
+appRoute.post(
+  'storage.saveFile',
+  config.proxyPrefixPath.uri + '/storage/saveFile/:semester/:courseCode/:rounds',
+  Admin.saveFileToStorage
+)
+appRoute.post(
+  'storage.updateFile',
+  config.proxyPrefixPath.uri + '/storage/updateFile/:fileName/',
+  Admin.updateFileInStorage
+)
+appRoute.post(
+  'storage.deleteFile',
+  config.proxyPrefixPath.uri + '/storage/deleteFile/:fileName',
+  Admin.deleteFileInStorage
+)
 server.use('/', appRoute.getRouter())
 
 // Not found etc
