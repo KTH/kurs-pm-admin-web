0cddc9f92876de4cc0656e3618d2aa30a008e435
Elena Rakhimova <elenara@kth.se>
2020-11-19 15:26:46 +0100

    fix: add test for AdminPage in status 'new'

--- test/unit/views/AdminPage.test.js
+++ test/unit/views/AdminPage.test.js
@@ -4,50 +4,46 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import i18n from '../../../i18n'
 import { StaticRouter } from 'react-router'
-import MemoMenu from '../../../public/js/app/components/MemoMenu'
+import AdminPage from '../../../public/js/app/views/AdminPage'
 import mockRouterStore from '../../mocks/mockRouterStore'
 const { getAllByRole, getAllByTestId, getAllByText, getByTestId, getByText } = screen
 
-const RenderMemoMenu = ({ userLang = 'en', status = 'new', ...rest }) => {
+const RenderAdminPage = ({ userLang = 'en', status = 'new', ...rest }) => {
   const rS = mockRouterStore(userLang, status)
   return (
-    <MemoMenu
-      routerStore={rS}
-      {...rest}
-      semesterList={rS.semesters}
-      roundList={rS.roundData}
-      activeSemester="20192"
-      firstVisit={true}
-    />
+    <StaticRouter>
+      <Provider routerStore={rS}>
+        <AdminPage
+          semesterList={rS.semesters}
+          firstVisit={true}
+          roundList={rS.roundData}
+          activeSemester="20192"
+          {...rest}
+        />
+      </Provider>
+    </StaticRouter>
   )
 }
 
-describe('User language: English. Component <RenderMemoMenu>', () => {
+describe('User language: English. Component <AdminPage> with status and progress "new"', () => {
   beforeEach(() => {
-    render(<RenderMemoMenu userLang="en" />)
+    render(<RenderAdminPage userLang="en" status="new" />)
   })
   test('renders a course development page', done => {
     done()
   })
   test('renders headers h3', () => {
     const allH3Headers = getAllByRole('heading', { level: 3 })
-    expect(allH3Headers.length).toBe(2)
+    expect(allH3Headers.length).toBe(1)
     expect(allH3Headers[0]).toHaveTextContent('Välj termin')
-    expect(allH3Headers[1]).toHaveTextContent('Välj kursomgång')
   })
 
   test('renders buttons. English.', () => {
     const buttons = getAllByRole('button')
-    expect(buttons.length).toBe(4)
+    expect(buttons.length).toBe(3)
     expect(buttons[0]).toHaveTextContent('')
     expect(buttons[1]).toHaveTextContent('Välj termin')
-    expect(buttons[2]).toHaveTextContent('')
-    expect(buttons[3]).toHaveTextContent('Avbryt')
-  })
-
-  test('renders checkbox for course offering which does not have a published course data', () => {
-    const checkboxes = getAllByRole('checkbox')
-    expect(checkboxes.length).toBe(2)
+    expect(buttons[2]).toHaveTextContent('Avbryt')
   })
 
   test('renders HT 2019 dropdown elements', () => {
