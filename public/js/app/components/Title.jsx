6719bcd96e1ecd6bfb3e69c295e5d2c677dbf040
Elena Rakhimova <elenara@kth.se>
2021-10-07 10:25:41 +0200

    refactor: use PageHeading from component library

--- public/js/app/components/Title.jsx
+++ public/js/app/components/Title.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import ProgressBar from '../components/ProgressBar'
+import PropTypes from 'prop-types'
+import { ProgressBar } from '@kth/kth-reactstrap/dist/components/utbildningsinfo'
+import { PageHeading } from '@kth/kth-reactstrap/dist/components/studinfo'
+import { Alert } from 'reactstrap'
 import i18n from '../../../../i18n/index'
 
 const ADMIN_COURSE_PM_DATA = '/kursinfoadmin/kurs-pm-data/'
@@ -7,26 +10,26 @@ const ADMIN_COURSE_PM_DATA = '/kursinfoadmin/kurs-pm-data/'
 const showInfoKTHrecommendation = (courseCode, langIndex) => {
   const langAbbr = langIndex === 0 ? 'en' : 'sv'
   const {
-    after_create_memo_link,
-    alert_header,
-    before_create_memo_link,
-    label_create_memo_link,
-    label_kurs_pm_web_link,
+    after_create_memo_link: labelAfterCreateMemoLink,
+    alert_header: alertHeader,
+    before_create_memo_link: labelBeforeCreateMemoLink,
+    label_create_memo_link: labelCreateMemoLink,
+    label_kurs_pm_web_link: labelCourseMemoWebLink,
   } = i18n.messages[langIndex].messages.alert_recommendation
 
   return (
-    <div className="alert alert-info" role="alert">
-      <h4>{alert_header}</h4>
+    <Alert color="info" className="alert-margin">
+      <h4>{alertHeader}</h4>
       <p>
-        {`${before_create_memo_link} `}
-        <a href={`${ADMIN_COURSE_PM_DATA}${courseCode}?l=${langAbbr}`}>{label_create_memo_link}</a>
-        {` ${after_create_memo_link} `}
+        {`${labelBeforeCreateMemoLink} `}
+        <a href={`${ADMIN_COURSE_PM_DATA}${courseCode}?l=${langAbbr}`}>{labelCreateMemoLink}</a>
+        {` ${labelAfterCreateMemoLink} `}
         <a href="https://intra.kth.se/utbildning/utveckling-och-hogskolepedagogik/stodmaterial/kurs-pm">
-          {label_kurs_pm_web_link}
+          {labelCourseMemoWebLink}
         </a>
-        {'.'}
+        .
       </p>
-    </div>
+    </Alert>
   )
 }
 
@@ -38,17 +41,22 @@ const Title = ({ courseCode, header, title, language: langIndex, progress, showP
   const courseName = `${courseCode} ${courseTitle} ${creditUnit}`
 
   return (
-    <header id="course-title" className="pageTitle">
-      {title && (
-        <span id="page-course-title" role="heading" aria-level="1">
-          <span className="t1">{header}</span>
-          <span className="t4">{courseCode && courseName}</span>
-        </span>
-      )}
+    <div>
+      <PageHeading subHeading={courseCode && courseName}>{header}</PageHeading>
       {progress === 1 && showInfoKTHrecommendation(courseCode, langIndex)}
-      {showProgressBar && <ProgressBar language={langIndex} active={progress} />}
-    </header>
+      {showProgressBar && <ProgressBar active={progress} pages={i18n.messages[langIndex].messages.pagesProgressBar} />}
+    </div>
   )
+}
+
+Title.propTypes = {
+  courseCode: PropTypes.string.isRequired,
+  language: PropTypes.oneOf([0, 1]).isRequired,
+  title: PropTypes.oneOf([
+    '',
+    PropTypes.string,
+    PropTypes.shape({ credits: PropTypes.oneOf([PropTypes.string, PropTypes.number]), name: PropTypes.string }),
+  ]).isRequired,
 }
 
 export default Title
