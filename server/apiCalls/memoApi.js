25b47420fb50de018c306f5bfccc285af2d685dc
Elena Rakhimova <elenara@kth.se>
2022-04-19 18:05:54 +0200

    refactor: use short obj

--- server/apiCalls/memoApi.js
+++ server/apiCalls/memoApi.js
@@ -13,31 +13,31 @@ async function _getMemoData(id) {
   const paths = api.memoApi.paths
   // console.log('_getRoundData', paths)
   const client = api.memoApi.client
-  const uri = client.resolve(paths.getCourseRoundMemoDataById.uri, { id: id })
-  return client.getAsync({ uri: uri })
+  const uri = client.resolve(paths.getCourseRoundMemoDataById.uri, { id })
+  return client.getAsync({ uri })
 }
 
 async function _setMemoData(id, sendObject) {
   const paths = api.memoApi.paths
   // console.log('_setRoundData', sendObject)
   const client = api.memoApi.client
-  const uri = client.resolve(paths.postCourseMemoData.uri, { id: id })
-  return client.postAsync({ uri: uri, body: sendObject })
+  const uri = client.resolve(paths.postCourseMemoData.uri, { id })
+  return client.postAsync({ uri, body: sendObject })
 }
 
 async function _deleteMemoData(id) {
   const paths = api.memoApi.paths
   const client = api.memoApi.client
-  const uri = client.resolve(paths.deleteCourseRoundMemoDataById.uri, { id: id })
-  return client.delAsync({ uri: uri })
+  const uri = client.resolve(paths.deleteCourseRoundMemoDataById.uri, { id })
+  return client.delAsync({ uri })
 }
 
 async function _getUsedRounds(courseCode, semester) {
   try {
     const paths = api.memoApi.paths
     const client = api.memoApi.client
-    const uri = client.resolve(paths.getUsedRounds.uri, { courseCode: courseCode, semester: semester })
-    return await client.getAsync({ uri: uri })
+    const uri = client.resolve(paths.getUsedRounds.uri, { courseCode, semester })
+    return await client.getAsync({ uri })
   } catch (error) {
     return error
   }
