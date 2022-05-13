31832925c6e074c088672d7eb9ee8318fb249ed0
Elena Rakhimova <elenara@kth.se>
2022-05-13 17:30:15 +0200

    refactor: divide context in more parts common, client, server

--- public/js/app/client-context/addClientFunctionsToWebContext.js
+++ public/js/app/client-context/addClientFunctionsToWebContext.js
@@ -4,33 +4,7 @@
 
 // eslint-disable-next-line no-unused-vars
 import axios from 'axios'
-import { getAccess } from '../util/helpers'
-
-const paramRegex = /\/(:[^\/\s]*)/g
-
-function _paramReplace(path, params) {
-  let tmpPath = path
-  const tmpArray = tmpPath.match(paramRegex)
-  tmpArray &&
-    tmpArray.forEach(element => {
-      tmpPath = tmpPath.replace(element, '/' + params[element.slice(2)])
-    })
-  return tmpPath
-}
-
-function buildApiUrl(path, params) {
-  let host
-  if (typeof window !== 'undefined') {
-    host = this.apiHost
-  } else {
-    host = 'http://localhost:' + this.browserConfig.port
-  }
-  if (host[host.length - 1] === '/') {
-    host = host.slice(0, host.length - 1)
-  }
-  const newPath = params ? _paramReplace(path, params) : path
-  return [host, newPath].join('')
-}
+import { createCommonContextFunctions } from '../../../../common-context/createCommonContextFunctions'
 
 /** ***************************************************************************************************************************************** */
 /*                                                       FILE STORAGE ACTIONS                                                      */
@@ -128,82 +102,21 @@ function getCourseInformation(courseCode, userName, lang = 'sv') {
     })
 }
 
-/** ***************************************************************************************************************************************** */
-/*                                             HANDLE DATA FROM API    (DUPLICATED IN SERVER SIDE AS WELL)                                                              */
-/** ***************************************************************************************************************************************** */
-
-function handleCourseData(courseObject, courseCode, userName, language) {
-  // Building up courseTitle, courseData, semesters and roundData
-  if (courseObject === undefined) {
-    this.errorMessage = 'Whoopsi daisy... kan just nu inte hämta data från kopps'
-    return undefined
-  }
-  try {
-    this.courseData = {
-      courseCode,
-      gradeScale: courseObject.formattedGradeScales,
-      semesterObjectList: {},
-    }
-    this.courseTitle = {
-      name: courseObject.course.title[this.language === 0 ? 'en' : 'sv'],
-      credits:
-        courseObject.course.credits.toString().indexOf('.') < 0
-          ? courseObject.course.credits + '.0'
-          : courseObject.course.credits,
-    }
-
-    for (let semester = 0; semester < courseObject.termsWithCourseRounds.length; semester++) {
-      this.courseData.semesterObjectList[courseObject.termsWithCourseRounds[semester].term] = {
-        rounds: courseObject.termsWithCourseRounds[semester].rounds,
-      }
-    }
-
-    const thisStore = this
-    courseObject.termsWithCourseRounds.map(semester => {
-      if (thisStore.semesters.indexOf(semester.term) < 0) thisStore.semesters.push(semester.term)
-
-      if (!thisStore.roundData.hasOwnProperty(semester.term)) {
-        thisStore.roundData[semester.term] = []
-        thisStore.roundAccess[semester.term] = {}
-      }
-
-      thisStore.roundData[semester.term] = semester.rounds.map(round => {
-        return (round.ladokRoundId = {
-          courseCode: this.courseCode,
-          roundId: round.ladokRoundId,
-          language: round.language[language],
-          shortName: round.shortName,
-          startDate: round.firstTuitionDate,
-          ladokUID: round.ladokUID,
-          hasAccess: getAccess(this.member, round, this.courseCode, semester.term),
-        })
-      })
-    })
-  } catch (err) {
-    if (err.response) {
-      throw new Error(err.message)
-    }
-    throw err
-  }
-}
-
 function createMemoData(semester, rounds) {
   // Creates a list with memo object with information from selected rounds
   this.status = 'new'
-  let newMemo = {}
   this.newMemoList = []
   this.activeSemester = semester
-  let id = ''
 
   for (let round = 0; round < rounds.length; round++) {
     const id = `${this.courseData.courseCode}_${semester}_${rounds[round]}`
     const newMemo = {
       _id: id,
       courseMemoFileName: '',
-      changedBy: this.user,
+      changedBy: this.username,
       courseCode: this.courseData.courseCode,
       pdfMemoUploadDate: '',
-      semester: semester,
+      semester,
       koppsRoundId: rounds[round],
     }
     this.newMemoList.push(newMemo)
@@ -211,26 +124,15 @@ function createMemoData(semester, rounds) {
   return this.newMemoList
 }
 
-function getMemberOf(memberOf = [], id, userName, superUser) {
-  if (id.length > 7) {
-    let splitId = id.split('_')
-    this.courseCode = splitId[0].length > 12 ? id.slice(0, 7).toUpperCase() : id.slice(0, 6).toUpperCase()
-  } else {
-    this.courseCode = id.toUpperCase()
-  }
-  this.member = memberOf.filter(member => member.indexOf(this.courseCode) > -1 || member.indexOf(superUser) > -1)
-  this.user = userName
-}
-
 function addClientFunctionsToWebContext() {
   const functions = {
-    buildApiUrl,
     createMemoData,
     deleteFileInStorage,
     getCourseInformation,
     getUsedRounds,
     postMemoData,
     updateFileInStorage,
+    ...createCommonContextFunctions(),
   }
   return functions
 }
--- server/ssr-context/createServerSideContext.js
+++ server/ssr-context/createServerSideContext.js
@@ -1,130 +1,17 @@
 /* eslint no-use-before-define: ["error", "nofunc"] */
 
 // @ts-check
+const { createCommonContextFunctions } = require('../../common-context/createCommonContextFunctions')
 
-// eslint-disable-next-line no-unused-vars
+function setMemberInfo(loggedInUser, courseCode, username) {
+  this.courseCode = courseCode
+  const { memberOf, roles } = loggedInUser
 
-const SUPERUSER_PART = 'kursinfo-admins'
-
-const getAccess = (memberOf, round, courseCode, semester) => {
-  if (
-    memberOf.toString().indexOf(courseCode.toUpperCase() + '.examiner') > -1 ||
-    memberOf.toString().indexOf(SUPERUSER_PART) > -1
-  ) {
-    return true
+  this.member = {
+    memberOfCourseRelatedGroups: memberOf.filter(member => member.indexOf(this.courseCode) > -1),
+    otherRoles: roles,
   }
-
-  if (
-    memberOf.toString().indexOf(`${courseCode.toUpperCase()}.${semester}.${round.ladokRoundId}.courseresponsible`) > -1
-  ) {
-    return true
-  }
-
-  if (memberOf.toString().indexOf(`${courseCode.toUpperCase()}.${semester}.${round.ladokRoundId}.teachers`) > -1) {
-    return true
-  }
-
-  return false
-}
-const paramRegex = /\/(:[^\/\s]*)/g
-
-function _paramReplace(path, params) {
-  let tmpPath = path
-  const tmpArray = tmpPath.match(paramRegex)
-  tmpArray &&
-    tmpArray.forEach(element => {
-      tmpPath = tmpPath.replace(element, '/' + params[element.slice(2)])
-    })
-  return tmpPath
-}
-
-function buildApiUrl(path, params) {
-  let host
-  if (typeof window !== 'undefined') {
-    host = this.apiHost
-  } else {
-    host = 'http://localhost:' + this.browserConfig.port
-  }
-  if (host[host.length - 1] === '/') {
-    host = host.slice(0, host.length - 1)
-  }
-  const newPath = params ? _paramReplace(path, params) : path
-  return [host, newPath].join('')
-}
-
-/** ***************************************************************************************************************************************** */
-/*                                                       FILE STORAGE ACTIONS                                                      */
-/** ***************************************************************************************************************************************** */
-
-/** ***************************************************************************************************************************************** */
-/*                                                     HANDLE DATA FROM API                                                                  */
-/** ***************************************************************************************************************************************** */
-
-function handleCourseData(courseObject, courseCode, userName, language) {
-  // Building up courseTitle, courseData, semesters and roundData
-  if (courseObject === undefined) {
-    this.errorMessage = 'Whoopsi daisy... kan just nu inte hämta data från kopps'
-    return undefined
-  }
-  try {
-    // console.log('courseObject', courseObject)
-    this.courseData = {
-      courseCode,
-      gradeScale: courseObject.formattedGradeScales,
-      semesterObjectList: {},
-    }
-    this.courseTitle = {
-      name: courseObject.course.title[this.language === 0 ? 'en' : 'sv'],
-      credits:
-        courseObject.course.credits.toString().indexOf('.') < 0
-          ? courseObject.course.credits + '.0'
-          : courseObject.course.credits,
-    }
-
-    for (let semester = 0; semester < courseObject.termsWithCourseRounds.length; semester++) {
-      this.courseData.semesterObjectList[courseObject.termsWithCourseRounds[semester].term] = {
-        rounds: courseObject.termsWithCourseRounds[semester].rounds,
-      }
-    }
-
-    const thisStore = this
-    courseObject.termsWithCourseRounds.map(semester => {
-      if (thisStore.semesters.indexOf(semester.term) < 0) thisStore.semesters.push(semester.term)
-
-      if (!thisStore.roundData.hasOwnProperty(semester.term)) {
-        thisStore.roundData[semester.term] = []
-        thisStore.roundAccess[semester.term] = {}
-      }
-
-      thisStore.roundData[semester.term] = semester.rounds.map(round => {
-        return (round.ladokRoundId = {
-          courseCode: this.courseCode,
-          roundId: round.ladokRoundId,
-          language: round.language[language],
-          shortName: round.shortName,
-          startDate: round.firstTuitionDate,
-          ladokUID: round.ladokUID,
-          hasAccess: getAccess(this.member, round, this.courseCode, semester.term),
-        })
-      })
-    })
-  } catch (err) {
-    if (err.response) {
-      throw new Error(err.message)
-    }
-    throw err
-  }
-}
-
-function getMemberOf(memberOf = [], id, userName, superUser) {
-  if (id.length > 7) {
-    let splitId = id.split('_')
-    this.courseCode = splitId[0].length > 12 ? id.slice(0, 7).toUpperCase() : id.slice(0, 6).toUpperCase()
-  } else {
-    this.courseCode = id.toUpperCase()
-  }
-  this.member = memberOf.filter(member => member.indexOf(this.courseCode) > -1 || member.indexOf(superUser) > -1)
-  this.user = userName
+  this.username = username
 }
 
 function setLanguage(lang = 'sv') {
@@ -156,11 +43,10 @@ function createServerSideContext() {
     roundAccess: {},
     user: '',
     activeSemester: '',
-    buildApiUrl,
-    getMemberOf,
-    handleCourseData,
+    setMemberInfo,
     setBrowserConfig,
     setLanguage,
+    ...createCommonContextFunctions(),
   }
   return context
 }
