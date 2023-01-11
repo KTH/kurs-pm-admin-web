383566916c1ec0dcf42b863d5bb123e2ce9b50c2
samtal <talic@kth.se>
2023-01-11 10:00:57 +0100

    fix:(KUI-927) update date format

--- public/js/app/util/helpers.js
+++ public/js/app/util/helpers.js
@@ -2,12 +2,21 @@
 
 import i18n from '../../../../i18n/index'
 
+const formatToLocaleDate = date => {
+  if (date === '') return null
+  const timestamp = Date.parse(date)
+  const parsedDate = new Date(timestamp)
+  const options = { day: 'numeric', month: 'short', year: 'numeric' }
+  const languageTag = 'en-GB'
+
+  return parsedDate.toLocaleDateString(languageTag, options)
+}
+
 const getDateFormat = (date, language) => {
   if (language === 'Svenska' || language === 'Engelska' || language === 1 || language === 'sv') {
     return date
   }
-  const splitDate = date.split('-')
-  return `${splitDate[2]}/${splitDate[1]}/${splitDate[0]}`
+  return formatToLocaleDate(date)
 }
 
 const getTodayDate = (date = '') => {
